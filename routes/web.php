b4375b80540f4ace2e9f1f861a4df1df7341cc6f
AMOUCHAL Moustapha <amouchaldev@gmail.com>
2024-07-13 15:05:04 +0100

    define web and api routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([
+    'as' => 'products.'
+], function () {
+    Route::view('/', 'products.index')->name('index');
+    Route::view('/products/create', 'products.create')->name('create');
 });
